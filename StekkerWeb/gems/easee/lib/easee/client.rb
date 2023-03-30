e7d45ecee8bf0b5cd81672817926e77448f470ca
Bob Forma <bforma@users.noreply.github.com>
2023-03-30 14:56:51 +0200

    Automatically refresh access token

--- StekkerWeb/gems/easee/lib/easee/client.rb
+++ StekkerWeb/gems/easee/lib/easee/client.rb
@@ -2,10 +2,10 @@ module Easee
   class Client
     BASE_URL = "https://api.easee.cloud".freeze
 
-    def initialize(user_name:, password:)
+    def initialize(user_name:, password:, token_store: ThreadSafe::Cache.new)
       @user_name = user_name
       @password = password
-      @access_token = nil
+      @token_store = token_store
     end
 
     # https://developer.easee.cloud/reference/post_api-chargers-id-unpair
@@ -53,27 +53,66 @@ module Easee
     end
 
     def get(endpoint, query = {})
-      authenticated_connection.get("#{BASE_URL}#{endpoint}", query)
-    rescue Faraday::Error => e
-      raise Errors::RequestFailed, "Request returned status #{e.response_status}"
+      with_error_handling do
+        authenticated_connection.get("#{BASE_URL}#{endpoint}", query)
+      end
     end
 
     def post(endpoint, body: nil, query: nil)
-      authenticated_connection.post("#{BASE_URL}#{endpoint}", body) do |req|
-        req.params = query unless query.nil?
+      with_error_handling do
+        authenticated_connection.post("#{BASE_URL}#{endpoint}", body) do |req|
+          req.params = query unless query.nil?
+        end
+      end
+    end
+
+    def with_error_handling
+      token_refreshed ||= false
+
+      yield
+    rescue Faraday::UnauthorizedError => e
+      if token_refreshed
+        raise Errors::RequestFailed, "Request returned status #{e.response_status}"
+      else
+        refresh_access_token!
+        token_refreshed = true
+
+        retry
       end
     rescue Faraday::Error => e
       raise Errors::RequestFailed, "Request returned status #{e.response_status}"
     end
 
     def access_token
-      @access_token ||= request_access_token
+      @token_store
+        .fetch_or_store(:tokens) { request_access_token }
+        .fetch("accessToken")
+    end
+
+    def refresh_access_token!
+      @token_store.put(:tokens, refresh_access_token)
+    rescue Faraday::Error => e
+      raise Errors::RequestFailed, "Request returned status #{e.response_status}"
     end
 
     # https://developer.easee.cloud/reference/post_api-accounts-login
     def request_access_token
-      connection.post("/api/accounts/login", userName: user_name, password:)
-        .then { |response| response.body.fetch("accessToken") }
+      connection
+        .post("/api/accounts/login", userName: user_name, password:)
+        .then(&:body)
+    end
+
+    # https://developer.easee.cloud/reference/post_api-accounts-refresh-token
+    def refresh_access_token
+      tokens = @token_store.fetch(:tokens)
+
+      connection
+        .post(
+          "/api/accounts/refresh_token",
+          accessToken: tokens.fetch("accessToken"),
+          refreshToken: tokens.fetch("refreshToken"),
+        )
+        .then(&:body)
     end
   end
 end
--- StekkerWeb/gems/easee/spec/easee/client_spec.rb
+++ StekkerWeb/gems/easee/spec/easee/client_spec.rb
@@ -1,13 +1,135 @@
 RSpec.describe Easee::Client do
-  describe "#pair" do
-    it "pairs a new charger" do
-      stub_token_request(user_name: "easee", password: "money")
+  describe "authentication" do
+    it "obtains and uses a new access token when none is provided" do
+      user_name = "easee"
+      password = "money"
+      token_store = ThreadSafe::Cache.new
+      tokens = { "accessToken" => "T123" }
+
+      stub_request(:post, "https://api.easee.cloud/api/accounts/login")
+        .with(
+          body: { userName: user_name, password: }.to_json,
+        )
+        .to_return(
+          status: 200,
+          body: tokens.to_json,
+          headers: { "Content-Type": "application/json" },
+        )
 
       stub_request(:post, "https://api.easee.cloud/api/chargers/123ABC/pair?pinCode=1234")
         .with(headers: { "Authorization" => "Bearer T123" })
         .to_return(status: 200, body: "")
 
-      client = Easee::Client.new(user_name: "easee", password: "money")
+      client = Easee::Client.new(user_name:, password:, token_store:)
+
+      expect { client.pair(charger_id: "123ABC", pin_code: "1234") }
+        .to change { token_store[:tokens] }
+        .from(nil)
+        .to(tokens)
+    end
+
+    it "refreshes the access token and uses the new one when it is expired" do
+      user_name = "easee"
+      password = "money"
+      token_store = ThreadSafe::Cache.new
+      current_tokens = { "accessToken" => "T123", "refreshToken" => "R456" }
+      token_store[:tokens] = current_tokens
+      new_tokens = { "accessToken" => "T789" }
+
+      stub_request(:post, "https://api.easee.cloud/api/chargers/123ABC/pair?pinCode=1234")
+        .to_return(
+          { status: 401, body: "Invalid token" },
+          { status: 200, body: "" },
+        )
+
+      stub_request(:post, "https://api.easee.cloud/api/accounts/refresh_token")
+        .with(
+          body: { accessToken: "T123", refreshToken: "R456" }.to_json,
+        )
+        .to_return(
+          status: 200,
+          body: new_tokens.to_json,
+          headers: { "Content-Type": "application/json" },
+        )
+
+      client = Easee::Client.new(user_name:, password:, token_store:)
+
+      expect { client.pair(charger_id: "123ABC", pin_code: "1234") }
+        .to change { token_store[:tokens] }
+        .from(current_tokens)
+        .to(new_tokens)
+    end
+
+    it "only tries to refresh the access token once" do
+      user_name = "easee"
+      password = "money"
+      token_store = ThreadSafe::Cache.new
+      current_tokens = { "accessToken" => "T123", "refreshToken" => "R456" }
+      token_store[:tokens] = current_tokens
+      new_tokens = { "accessToken" => "T789", "refreshToken" => "R654" }
+
+      stub_request(:post, "https://api.easee.cloud/api/chargers/123ABC/pair?pinCode=1234")
+        .to_return(
+          { status: 401, body: "Invalid token" },
+          { status: 401, body: "Invalid token" },
+        )
+
+      stub_request(:post, "https://api.easee.cloud/api/accounts/refresh_token")
+        .with(
+          body: { accessToken: "T123", refreshToken: "R456" }.to_json,
+        )
+        .to_return(
+          status: 200,
+          body: new_tokens.to_json,
+          headers: { "Content-Type": "application/json" },
+        )
+
+      client = Easee::Client.new(user_name:, password:, token_store:)
+
+      expect { client.pair(charger_id: "123ABC", pin_code: "1234") }.to raise_error(Easee::Errors::RequestFailed)
+    end
+
+    it "fails when the acess token could not be refreshed" do
+      user_name = "easee"
+      password = "money"
+      token_store = ThreadSafe::Cache.new
+      current_tokens = { "accessToken" => "T123", "refreshToken" => "R456" }
+      token_store[:tokens] = current_tokens
+
+      stub_request(:post, "https://api.easee.cloud/api/chargers/123ABC/pair?pinCode=1234")
+        .to_return(
+          status: 401, body: "Invalid token",
+        )
+
+      stub_request(:post, "https://api.easee.cloud/api/accounts/refresh_token")
+        .with(
+          body: { accessToken: "T123", refreshToken: "R456" }.to_json,
+        )
+        .to_return(
+          status: 401,
+          body: {
+            errorCode: 104,
+            errorCodeName: "InvalidRefreshToken",
+          }.to_json,
+          headers: { "Content-Type": "application/json" },
+        )
+
+      client = Easee::Client.new(user_name:, password:, token_store:)
+
+      expect { client.pair(charger_id: "123ABC", pin_code: "1234") }
+        .to raise_error(Easee::Errors::RequestFailed)
+    end
+  end
+
+  describe "#pair" do
+    it "pairs a new charger" do
+      token_store = ThreadSafe::Cache.new.tap { |x| x[:tokens] = { "accessToken" => "T123" } }
+
+      stub_request(:post, "https://api.easee.cloud/api/chargers/123ABC/pair?pinCode=1234")
+        .with(headers: { "Authorization" => "Bearer T123" })
+        .to_return(status: 200, body: "")
+
+      client = Easee::Client.new(user_name: "easee", password: "money", token_store:)
 
       expect { client.pair(charger_id: "123ABC", pin_code: "1234") }.not_to raise_error
     end
@@ -15,13 +137,13 @@ RSpec.describe Easee::Client do
 
   describe "#unpair" do
     it "unpairs a charger" do
-      stub_token_request(user_name: "easee", password: "money")
+      token_store = ThreadSafe::Cache.new.tap { |x| x[:tokens] = { "accessToken" => "T123" } }
 
       stub_request(:post, "https://api.easee.cloud/api/chargers/123ABC/unpair?pinCode=1234")
         .with(headers: { "Authorization" => "Bearer T123" })
         .to_return(status: 200, body: "")
 
-      client = Easee::Client.new(user_name: "easee", password: "money")
+      client = Easee::Client.new(user_name: "easee", password: "money", token_store:)
 
       expect { client.unpair(charger_id: "123ABC", pin_code: "1234") }.not_to raise_error
     end
@@ -32,7 +154,7 @@ RSpec.describe Easee::Client do
       now = Time.zone.local(2023, 3, 27, 15, 21)
       Timecop.freeze(now)
 
-      stub_token_request(user_name: "easee", password: "money")
+      token_store = ThreadSafe::Cache.new.tap { |x| x[:tokens] = { "accessToken" => "T123" } }
 
       stub_request(:get, "https://api.easee.cloud/api/chargers/C123/state")
         .with(headers: { "Authorization" => "Bearer T123" })
@@ -42,7 +164,7 @@ RSpec.describe Easee::Client do
           headers: { "Content-Type": "application/json" },
         )
 
-      client = Easee::Client.new(user_name: "easee", password: "money")
+      client = Easee::Client.new(user_name: "easee", password: "money", token_store:)
 
       state = client.state("C123")
 
@@ -59,13 +181,13 @@ RSpec.describe Easee::Client do
 
   describe "#pause_charging" do
     it "sends a pause_charging command" do
-      stub_token_request(user_name: "easee", password: "money")
+      token_store = ThreadSafe::Cache.new.tap { |x| x[:tokens] = { "accessToken" => "T123" } }
 
       stub_request(:post, "https://api.easee.cloud/api/chargers/C123/commands/pause_charging")
         .with(headers: { "Authorization" => "Bearer T123" })
         .to_return(status: 200, body: "")
 
-      client = Easee::Client.new(user_name: "easee", password: "money")
+      client = Easee::Client.new(user_name: "easee", password: "money", token_store:)
 
       expect { client.pause_charging("C123") }.not_to raise_error
     end
@@ -73,27 +195,15 @@ RSpec.describe Easee::Client do
 
   describe "#resume_charging" do
     it "sends a resume_charging command" do
-      stub_token_request(user_name: "easee", password: "money")
+      token_store = ThreadSafe::Cache.new.tap { |x| x[:tokens] = { "accessToken" => "T123" } }
 
       stub_request(:post, "https://api.easee.cloud/api/chargers/C123/commands/resume_charging")
         .with(headers: { "Authorization" => "Bearer T123" })
         .to_return(status: 200, body: "")
 
-      client = Easee::Client.new(user_name: "easee", password: "money")
+      client = Easee::Client.new(user_name: "easee", password: "money", token_store:)
 
       expect { client.resume_charging("C123") }.not_to raise_error
     end
-  end
-
-  def stub_token_request(user_name: "easee", password: "money")
-    stub_request(:post, "https://api.easee.cloud/api/accounts/login")
-      .with(
-        body: { userName: user_name, password: }.to_json,
-      )
-      .to_return(
-        status: 200,
-        body: { accessToken: "T123" }.to_json,
-        headers: { "Content-Type": "application/json" },
-      )
   end
 end
